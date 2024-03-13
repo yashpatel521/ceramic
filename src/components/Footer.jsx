0eb09f5700c621cd54f370705025152d8b5b3f50
Yash Patel <yash1451999@gmail.com>
2024-03-13 17:34:54 -0400

    nav footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,43 +3,49 @@ import "../assets/css/footer.css";
 function Footer() {
   return (
     <footer>
-      <div className="footer-first">
-        <img src="/img/sidelogo.png" alt="logo" srcSet="/img/sidelogo.png" />
-        <div className="footer-email">
-          <input type="text" placeholder="Your Email Address" />
-          <IoIosSend size={25} />
+      <div className="footer-backdrop">
+        <div className="footer-first">
+          <div className="d-flex align-items-center">
+            <img src="/img/logo.png" alt="logo" srcSet="/img/logo.png" />
+            <p>Ceramic & Tiles</p>
+          </div>
+
+          <div className="footer-email">
+            <input type="text" placeholder="Your Email Address" />
+            <IoIosSend size={25} />
+          </div>
         </div>
-      </div>
-      <div className="footer-second">
-        <div className="footer-aboutus">
-          <h3>About US</h3>
-          <p>
-            There are many variations of passages of Lorem the majority suffer
-          </p>
-          <p>+1800-001-658</p>
+        <div className="footer-second">
+          <div className="footer-aboutus">
+            <h3>About US</h3>
+            <p>
+              There are many variations of passages of Lorem the majority suffer
+            </p>
+            <p>+1800-001-658</p>
+          </div>
+          <div className="footer-ournavigation">
+            <h3>Our Navigation</h3>
+            <p>Marble</p>
+            <p>Marble</p>
+            <p>Marble</p>
+          </div>
+          <div className="footer-ourproduct">
+            <h3>Our Product</h3>
+            <p>marcelwhite</p>
+            <p>marcelwhite</p>
+            <p>marcelwhite</p>
+          </div>
+          <div className="footer-ourshowroom">
+            <h3>Our Showroom</h3>
+            <p>Themeforest, Envato HQ 24 Fifth st., Los Angeles, USA</p>
+            <p>+1800-001-658</p>
+            <p>Info@Peacefulthemes.Com</p>
+          </div>
         </div>
-        <div className="footer-ournavigation">
-          <h3>Our Navigation</h3>
-          <p>Marble</p>
-          <p>Marble</p>
-          <p>Marble</p>
+        <div className="footer-third">
+          <hr className="footer-hr" />
+          Copyright 2023 Ceramic tiles All Rights Reserved.
         </div>
-        <div className="footer-ourproduct">
-          <h3>Our Product</h3>
-          <p>marcelwhite</p>
-          <p>marcelwhite</p>
-          <p>marcelwhite</p>
-        </div>
-        <div className="footer-ourshowroom">
-          <h3>Our Showroom</h3>
-          <p>Themeforest, Envato HQ 24 Fifth st., Los Angeles, USA</p>
-          <p>+1800-001-658</p>
-          <p>Info@Peacefulthemes.Com</p>
-        </div>
-      </div>
-      <div className="footer-third">
-        <hr className="footer-hr" />
-        Copyright 2023 Ceramic tiles All Rights Reserved.
       </div>
     </footer>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,32 @@
 import { Link } from "react-router-dom";
 import "../assets/css/navbar.css";
 import { BiSolidPhoneCall } from "react-icons/bi";
-import { MdOutgoingMail } from "react-icons/md";
 function Navbar() {
   return (
     <nav>
-      <div className="nav-logo">
-        <img src="/img/logo.png" alt="Logo" srcSet="/img/logo.png" />
-        <p>Ceramic & Tiles</p>
-      </div>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/About">About Us</Link>
-          </li>
-          <li>
-            <Link to="/About">About Us</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="nav-subscribeEmail">
-        <div className="nav-searchicon mr-2">
-          <span>+1 1800-065-521</span>
-          <BiSolidPhoneCall size={25} />
+      <div className="nav-backdrop">
+        <div className="nav-logo">
+          <img src="/img/logo.png" alt="Logo" srcSet="/img/logo.png" />
+          <p>Ceramic & Tiles</p>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About Us</Link>
+            </li>
+            <li>
+              <Link to="/About">About Us</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-subscribeEmail">
+          <div className="nav-searchicon mr-2">
+            <span>+1 1800-065-521</span>
+            <BiSolidPhoneCall size={25} />
+          </div>
         </div>
       </div>
     </nav>
